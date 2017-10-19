c374c2d40c166ea815061da264fea68566678be8
samhoward13124 <32550365+samhoward13124@users.noreply.github.com>
2017-10-19 12:12:18 -0500

    Update User_Page.cs
    Hey, we got GitHub and this page to partially work!

--- KnoWhat/KnoWhat/User_Page.cs
+++ KnoWhat/KnoWhat/User_Page.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +9,36 @@ namespace KnoWhat
 {
     public class User_Page : ContentPage
     {
+        private ToolbarItem toolbar_Back;
+        private ToolbarItem toolbar_Search;
+        private Image user_Picture;
+
         public User_Page()
         {
-            Content = new StackLayout
+            Title = "Profile";
+            toolbar_Search = new ToolbarItem() { Icon = "search_icon.png" };
+            this.ToolbarItems.Add(toolbar_Search);
+            toolbar_Search.Clicked += toolbar_Search_Clicked;
+
+            var layout = new StackLayout();
+
+            Image user_Picture = new Image { Source = "userProfile.png" };
+
+            Entry post = new Entry
             {
-                Children = {
-                    new Label { Text = "Temp Profile Page" }
-                }
+                Placeholder = "Tell people about yourself.",
+                BackgroundColor = Color.White,
+                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.Fill
             };
+
+            layout.Children.Add(post);
+            Content = layout;
+        }
+        // Methods for when toolbar buttons are clicked 
+        void toolbar_Search_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new Categories_Page());
         }
     }
 }
